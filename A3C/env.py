993c9b8e6b4055950b65815623ac4865c01aee6e
Zixuiu <105110952+Zixuiu@users.noreply.github.com>
2023-08-09 22:32:36 +0800

    更新 env.py

--- A3C/env.py
+++ A3C/env.py
@@ -9,17 +9,31 @@ import numpy as np
 
 url = 'http://localhost/watermelon'  # "http://www.wesane.com/game/654"
 
-def process_frame42(frame):
+def process_frame42(frame): 
+    # 使用Python的切片语法，将输入图像裁剪为160x250的大小。这里，":250"表示取图像的最后250行，":160"表示取图像的前160列
+    # 这意味着我们只保留了原始图像的一个子区域，裁剪后的区域大小为160x250
+    # 然后，这个裁剪后的图像被赋值给变量frame，因此后续的操作都是在这个裁剪后的图像上进行的
     # 处理帧图像，将其裁剪为160x250大小，然后缩放为80x80
-    frame = frame[:250, :160]
-    frame = cv2.resize(frame, (80, 80))
-    cv2.imwrite("filename.png", frame)
-    frame = frame.mean(2, keepdims=True)
-    frame = frame.astype(np.float32)
-    frame *= (1.0 / 255.0)
-    frame = np.moveaxis(frame, -1, 0)
+    frame = frame[:250, :160] 
+    # 使用OpenCV的resize函数，将裁剪后的图像的大小调整为80x80。这个函数的第一个参数是源图像，第二个参数是目标大小
+    # 这个操作并不会改变图像的实际像素值，只是改变了图像的大小
+    frame = cv2.resize(frame, (80, 80)) 
+    # 使用OpenCV的imwrite函数，将处理后的图像保存为一个名为"filename.png"的文件。这个函数的第一个参数是源图像，第二个参数是文件名
+    cv2.imwrite("filename.png", frame) 
+    # 使用NumPy的mean函数，计算图像在颜色通道（即最后一个维度）上的平均值。这个函数的第一个参数是源数组，第二个参数是沿着哪个维度计算平均值。这里，我们设置keepdims=True，表示在计算平均值后保留原数组的维度
+    # 这个操作将每个像素的值转换为灰度值
+    frame = frame.mean(2, keepdims=True) 
+    # 使用NumPy的astype函数，将数组的数据类型转换为np.float32。这个函数的第一个参数是源数组，第二个参数是目标数据类型
+    # 这个操作将像素值转换为浮点数
+    frame = frame.astype(np.float32) 
+    # 使用NumPy的乘法运算符和常数1.0 / 255.0，将每个像素值乘以(1.0 / 255.0)，相当于将像素值归一化到[0, 1]区间内
+    # 这个操作将像素值归一化到[0, 1]区间内
+    frame *= (1.0 / 255.0) 
+    # 使用NumPy的moveaxis函数，将数组的维度顺序改变。这个函数的第一个参数是源数组，第二个参数是要移动的轴的索引，第三个参数是移动后的目标位置
+    # 这个操作将最后一个维度移到最前面，使得处理后的图像成为一个灰度图像
+    frame = np.moveaxis(frame, -1, 0) 
+    # 返回处理后的图像
     return frame
-
 class bigwaterlemon:
     def __init__(self):
         chrome_options = Options()
